69dfececd1bc0d6c813aa4f3c3d3e2c7f837458b
2rlo <wh0776283@gmail.com>
2023-01-18 15:38:24 +0900

    chore: sign up button

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GoogleLoginButton from "../components/GoogleLogin/GoogleLoginButton";
+import GoogleSignUpButton from "../components/GoogleSignUp/GoogleSIgnUpButton";
 
 function LoginPage() {
     return (
-        <div className="justify-center flex pt-96 pb-96">
+        <div className="justify-center flex pt-96 pb-80 flex-col items-center">
             <GoogleLoginButton></GoogleLoginButton>
+            <GoogleSignUpButton></GoogleSignUpButton>
         </div>
         // google sign up button
     )
