8162701db0d03cf6cdbd130175ec207ef406ad83
2rlo <wh0776283@gmail.com>
2023-02-04 15:36:42 +0900

    feat: google signUp skeleton

---
+++ src/views/GoogleSignUpCallbackPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleSignUpCallBack from "../components/GoogleSignUp/CoocleSignUpCallBack";
+
+function GoogleSignUpCallbackPage() {
+    return (
+        <>
+            <GoogleSignUpCallBack></GoogleSignUpCallBack>
+        </>
+    )
+}
+
+export default GoogleSignUpCallbackPage;
