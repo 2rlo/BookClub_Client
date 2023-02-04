54a4204d28e558b02748eda31e4dd8c8d0e59e97
2rlo <wh0776283@gmail.com>
2023-02-04 15:40:24 +0900

    fix: login click event

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,11 @@ function Navbar() {
     const onClickLog = () => {
         if (user_token !== null) {
             localStorage.removeItem("user_token");
-            navigate("/");
+            localStorage.removeItem("login_token");
+            window.location.replace("/");
+        }
+        else {
+            window.location.replace("/login");
         }
     }
 
@@ -38,7 +42,7 @@ function Navbar() {
                             >Grouping</a> : null}
                         </li>
                         <li>
-                            <a href={user_token ? null : "/login"} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" onClick={onClickLog}>{user_token ? "Logout" : "LogIn"}</a>
+                            <a className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" onClick={onClickLog}>{user_token ? "Logout" : "LogIn"}</a>
                         </li>
                     </ul>
                 </div>
